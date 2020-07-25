8177f68777d7b01a0cb2c2385cdd1b89b60ced75
Lucas Azevedo <lucas0294@gmail.com>
2020-07-24 23:33:07 -0300

    :package: menu sidebar

---
+++ Javascript/script.js
@@ -0,0 +1,7 @@
+var manulateral = document.querySelector(".menulateral");
+
+
+manulateral.addEventListener("click", function() {
+    document.querySelector(".container").classList.toggle("show-menu");
+
+});
--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # portfolio
+Meu portfolio de desenvolvedor
+
