2d7507e55b0da45678570abdf400f505e2d17cbf
Lucas Azevedo <lucas0294@gmail.com>
2020-08-05 12:40:09 -0300

    atualizacao

--- Javascript/script.js
+++ Javascript/script.js
@@ -5,3 +5,15 @@ manulateral.addEventListener("click", function() {
     document.querySelector(".container").classList.toggle("show-menu");
 
 });
+
+function typeWriter(elemento) {
+    const textArray = elemento.innerHTML.split('');
+    elemento.innerHTML = '';
+    textArray.forEach((letra, i) => {
+        setTimeout(() => elemento.innerHTML += letra, 100 * i)
+    });
+}
+
+
+const titulo = document.querySelector('h1');
+typeWriter(titulo);
